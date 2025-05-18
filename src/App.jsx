0108376e41f96cf6bdd5e676a2c39b66094c8e3b
Tommy Pearson <tundraperson@gmail.com>
2025-05-18 16:03:24 +0100

    first commit, functionality set, working

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import ToDoInput from "./components/ToDoInput";
+import ToDoList from "./components/ToDoList";
+import { useState, useEffect } from "react";
+
+function App() {
+
+  const [todos, setTodos] = useState(["Your first task..."]);
+  const [ todoValue, setTodoValue ] = useState("");
+
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }))
+  }
+
+  function handleAddTodos(newTodo) {
+    const newTodosList = [...todos, newTodo];
+    persistData(newTodosList);
+    setTodos(newTodosList);
+  }
+
+  function handleDeleteTodos(index) {
+    const newTodosList = todos.filter((todo, toDoIndex) => {
+      return toDoIndex !== index
+    });
+    persistData(newTodosList);
+    setTodos(newTodosList);
+  }
+
+  function handleEditTodos(index) {
+    const valueToEdit = todos[index]
+    setTodoValue(valueToEdit)
+    handleDeleteTodos(index);
+  }
+
+useEffect(() => {
+  if(!localStorage){
+    return
+  }
+  let localTodos = localStorage.getItem("todos");
+  if(!localTodos){
+    return
+  }
+
+  localTodos = JSON.parse(localTodos).todos
+  setTodos(localTodos)
+}, [todos]);
+
+  return (
+    <>
+     <ToDoInput todoValue={todoValue} setTodoValue={setTodoValue } handleAddTodos={handleAddTodos} />
+     <ToDoList handleEditTodos={handleEditTodos} handleDeleteTodos={handleDeleteTodos} todos={todos}/> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ToDoInput.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function ToDoInput(props) {
+    const { handleAddTodos, todoValue, setTodoValue } = props;
+
+    return (
+        <header>
+            <input value={todoValue} onChange={(e) => {setTodoValue(e.target.value)}} placeholder="What to do?..." />
+            <button onClick={() => {
+                handleAddTodos(todoValue)
+                setTodoValue("");}}>
+                Add
+            </button>
+        </header>
+    )
+}
